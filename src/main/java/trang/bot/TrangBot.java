e79378bac88f0221224f5dbfbe154a1b605e3063
leephongtrang <2031296@cegepmontpetit.ca>
2021-09-07 17:52:56 -0400

    PROGRES Implémentation de la gestion de profondeur
    Progression de la gestion de la profondeur a continuer.

--- src/main/java/trang/bot/TrangBot.java
+++ src/main/java/trang/bot/TrangBot.java
@@ -8,28 +8,32 @@ import java.io.IOException;
 
 public class TrangBot {
     public static void main(String[] args) {
-        //Debug
-        args = new String[3];
-        args[0] = "0";
-        args[1] = "https://jsoup.org/cookbook/input/load-document-from-url";
-        args[2] = "C:\\Users\\2031296\\Desktop\\Nouveau dossier";
-
+        //Paramètre pour debug
+        {
+            args = new String[3];
+            args[0] = "0";
+            args[1] = "https://jsoup.org/cookbook/input/load-document-from-url";
+            args[2] = "C:\\Users\\2031296\\Desktop\\Nouveau dossier";
+        }
 
         ValideNbParam(args);
         ValideParam(args);
+        Exploration(args);
     }
     public static void ValideNbParam(String[] tabl){
         if (tabl.length != 3) {
             System.out.println(
                 "Le nombre de paramètres donnés n'est pas correct.\n" +
                 "Il faut 3 paramètres :\n" +
-                "-La profondeurs d'exploration et doit être une nombre supérieur à 0\n" +
-                "-Un URL de départ valide\n" +
-                "-Un répertoire pour sauvegarder les pages téléchargées");
+                "-La profondeurs d'exploration et doit être une nombre supérieur à 0.\n" +
+                "-Un URL de départ valide.\n" +
+                "-Un répertoire pour sauvegarder les pages téléchargées.");
+            System.exit(0);
         }
     }
 
     public static void ValideParam(String[] tabl){
+        //Assignation des valeurs
         int Profondeur = Integer.parseInt(tabl[0]);
         String URL = tabl[1];
         File Dossier = new File(tabl[2]);
@@ -43,9 +47,8 @@ public class TrangBot {
 
         //Check l'URL
         {
-            Document doc = null;
             try {
-                doc = Jsoup.connect(URL).get();
+                Jsoup.connect(URL).get();
             } catch (IllegalArgumentException a) {
                 System.out.println("Le paramètre 2, l'URL n'est pas valide.");
             } catch (IOException e) {
@@ -65,7 +68,26 @@ public class TrangBot {
         }
     }
 
-    public static void Exploration(){
+    public static void Exploration(String[] pParam){
+        File Dossier = new File(pParam[2]);
 
+        Document Html = null;
+        try {
+            Html = Jsoup.connect(pParam[1]).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String NomHtml = Html.title();
+        String TextHtml = Html.outerHtml();
+
+        FileWriter FischierHtml = null;
+        try {
+            FischierHtml = new FileWriter(Dossier + "\\" + NomHtml + ".txt");
+            FischierHtml.write(TextHtml);
+            FischierHtml.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
